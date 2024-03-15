7968cdf5deb724b191fca76b2ed817b4c7febedc
AlexMois77 <159935651+AlexMois77@users.noreply.github.com>
2024-03-15 11:16:51 +0100

    1

---
+++ hw04_1.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+def total_salary(path):
+    try: 
+        full_path = Path(path) / "salary_file.txt" # додаємо лінк - вказаний шлях до файлу
+        
+        if full_path.exists() and full_path.is_file(): # Перевірка існування папки та файлу
+            
+            with open(full_path, "r", encoding='utf-8', errors='ignore') as fh: # відкриваємо файл 
+                lines = [el.strip() for el in fh.readlines()] # Розділяємо на строки
+                
+                obj_query = {}
+                total = 0
+                average = 0
+                count = 0
+            
+                for el in lines:
+                    key, value = el.split(',')     # розділюємо працівників і зп по комі
+                    total = total + int(value)     # сумуємо зп працівників
+                    count += 1                     # сумуємо кількість працівників
+                    obj_query.update({key: value}) # збираємо в словник ім'я і зп - хай буде
+                average = int(total/count)         # середня зп
+                korteg = (total, average)          # створюємо кортежик
+                print(f"Загальна сума заробітної плати: {korteg[0]}, Середня заробітна плата: {korteg[1]}")
+            return korteg
+        else:
+            return "Перевірте вказаний шлях та ім'я файла (повинно бути: salary_file.txt)"
+    except OSError as err:
+        return f('Помилка доступу до файлу', {err})
+
+total_salary("E:\piton_kurs\kurs2024\homework\HW4")
+
+
---
+++ hw04_2.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+def get_cats_info(path):
+    try:
+        full_path = Path(path) / "cats_file.txt" # додаємо лінк - вказаний шлях до файлу
+
+        if full_path.exists() and full_path.is_file(): # Перевіряємо чи є така дерікторія і чи є файл
+        
+            with open(full_path, "r", encoding='utf-8', errors='ignore') as fh: # відкриваємо файл 
+                
+                lines = [el.strip() for el in fh.readlines()] # Розділяємо на строки
+                
+                dict_total = {}
+                cats_info  = []
+
+                for el in lines:
+                    id, name, age = el.split(',')   # висмикуємо данні про кошаків
+                    dict_total["id"] = id           # закидаємо данні про кошаків: АйДі
+                    dict_total["name"] = name       # закидаємо данні про кошаків: Ім'я
+                    dict_total["age"] = age         # закидаємо данні про кошаків: на скільки досвідчений котик
+                    cats_info.append(str(dict_total)) # збираємо данні про кошаків в список
+            return cats_info
+        else:
+            return "Перевірте вказаний шлях та ім'я файла (повинно бути: salary_file.txt)"
+    except OSError as err:
+        return f('Помилка доступу до файлу', {err})
+
+get_cats_info("E:\piton_kurs\kurs2024\homework\HW4")
